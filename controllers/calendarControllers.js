52ef12a1dc1d468c4724fe7d42c3bcb7c27a0904
SunaryStudios <SunaryStudios@gmail.com>
2024-10-06 10:51:15 -0500

    update: FindOneAndUpdate

--- controllers/calendarControllers.js
+++ controllers/calendarControllers.js
@@ -4,18 +4,9 @@ exports.dayProblemCreate = async (req, res) => {
     try {
         const { day } = req.body;
 
-        console.log('Día creado', day)
-
         if (!day) {
             return res.status(400).json({ message: 'Datos del día no proporcionados' });
         }
-
-        // Encontrar todos los usuarios que tengan el evento en la misma fecha
-        const usersWithEvent = await User.find({ 
-            "events.day": day.day, 
-            "events.month": day.month, 
-            "events.year": day.year 
-        }).exec();
 
         const newEvent = {
             day: day.day,
@@ -23,41 +14,24 @@ exports.dayProblemCreate = async (req, res) => {
             year: day.year,
             response: day.response
         };
+        
+        const user = await User.findOneAndUpdate(
+            { key: day.user, "events.day": day.day, "events.month": day.month, "events.year": day.year },
+            { $set: { "events.$": newEvent } },
+            { new: true, upsert: true } 
+        );
 
-        if (usersWithEvent.length > 0) {
-            // Si existen usuarios con el mismo evento, actualizamos el evento
-            for (const user of usersWithEvent) {
-                const existingEventIndex = user.events.findIndex(event =>
-                    event.day === day.day &&
-                    event.month === day.month &&
-                    event.year === day.year
-                );
-
-                if (existingEventIndex !== -1) {
-                    user.events[existingEventIndex] = newEvent;
-                }
-
-                await user.save(); // Guardar los cambios
-            }
+        if (user) {
+            return res.status(200).json({ message: 'Evento guardado exitosamente', day: newEvent });
         } else {
-            // Si no existe el evento en ningún usuario, buscar el usuario por clave y agregar el nuevo evento
-            const user = await User.findOne({ key: day.user }).exec();
-
-            if (user) {
-                user.events.push(newEvent);
-                await user.save();
-            } else {
-                return res.status(404).json({ message: 'Usuario no encontrado' });
-            }
+            return res.status(404).json({ message: 'Usuario no encontrado' });
         }
-
-        res.status(200).json({ message: 'Evento guardado exitosamente', day: newEvent });
-
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Error en el servidor', error });
     }
 };
+
 
 exports.dayRequest = async (req, res) => {
     try {
